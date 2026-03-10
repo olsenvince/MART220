6e5e1779ef1dc06c83832199b657d3dc955b5731
Vince <156894363+olsenvince@users.noreply.github.com>
2026-03-10 03:36:02 -0600

    Update sketch.js

--- week 4/js/sketch.js
+++ week 4/js/sketch.js
@@ -1,14 +1,20 @@
+//character and environment assets from https://www.gameart2d.com/free-platformer-game-tileset.html
+
 let sheetImg;
+let background;
 
 function preload(){
-    sheetImg = loadImg()
+    background = loadImg(bg.png)
+    sheetImg = loadImg("tilesetImages/1.png")
 }
 function setup(){
-    createCanvas(400,400);
+    createCanvas(windowWidth, windowHeight, "pixelated x4");
+    //world.gravity.y = 10;
+    background(bg);
 }
 
 
 function draw(){
-    background (220);
+    
 }
 
